c7bc444d97c10f133167ec2c880b4942cee35f09
miftahulhaeri31-gif <miftahulhaeri31@gmail.com>
2026-05-04 19:32:47 -0700

    materi4.php

--- materi4.php
+++ materi4.php
@@ -24,3 +24,41 @@ Nama : <input type="text" name="nama">
 Email : <input type="email" name="email">
 <input type="submit" value="Kirim Data" name="kirim">
 </form>
+
+//Menampilkan Data dalam Tabel
+
+<table border="1">
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Aksi</th>
+    </tr>
+<?php
+$sql = "SELECT * FROM user";
+$query = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_assoc($query)) {
+    echo "<tr>
+    <td>{$row['id']}</td>
+    <td>{$row['username']}</td>
+    <td>{$row['password']}</td>
+    <td>{$row['nama']}</td>
+    <td>{$row['email']}</td>
+    <td><a href='materi4.php?hapus={$row['id']}'>Hapus<a> | <a href=?edit={$row['id']}>Edit</a> </td>
+    </tr>";
+}
+//proses hapus
+if (isset($_GET['hapus'])) {
+    echo "Data berhasil dihapus";
+    $id = $_GET['hapus'];
+    $sql = "DELETE FROM user WHERE id = '$id'";
+    $query = mysqli_query($conn, $sql);
+    if ($query) {
+        echo "Data berhasil dihapus";
+    } else {
+        echo "Data berhasil diedit";
+    }
+}
+?>
